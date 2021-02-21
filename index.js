84c0cd73fdf9c63116ea5c9e6098cc3468d0cc4e
irtizabatool <irtizabatool@gmail.com>
2021-02-21 23:48:17 +0530

    Used MiddleWare

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path = require('path');
 const members = require('./Members');
+const logger = require('./middleware/logger');
 
 const app = express();
+
+
+//Init middleware
+//app.use(logger);
 
 //Gets all members
 app.get('/api/members', (req, res) => res.json(members));
